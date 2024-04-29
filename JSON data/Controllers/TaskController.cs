d503fe7042b03d9d32e527ae2d38b045ea252191
Achyut <agaihr10@gmail.com>
2024-04-29 12:28:52 +0545

    Completed the program that takes uploaded image from client side and saves it with random GUID

--- JSON data/Controllers/TaskController.cs
+++ JSON data/Controllers/TaskController.cs
@@ -1,15 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JSON_data.Helpers;
+using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
 namespace JSON_data.Controllers
 {
+	[Route("api/[controller]")]
+	[ApiController]
 	public class TaskController : Controller
 	{
+		private IWebHostEnvironment _env;
+		private FileSaver _fileSaver;	
+
+		public TaskController(IWebHostEnvironment env, FileSaver fileSaver)
+		{
+			_env = env;
+			_fileSaver = new FileSaver(_env);	
+		}
+
+
 		[HttpPost("Upload")]
 		public async Task<IActionResult> uploadFile([FromForm] IFormFile file)
 		{
 			try
 			{
+				//calling file saver function
+				_fileSaver.FileSaveAsync(file, "assets/image");
 				return Ok("You have uploaded your file successfully");
 			}
 			catch(Exception ex)
