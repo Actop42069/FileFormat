ae68880abf13e8f40b8ee87805148ffda216ee82
Achyut <agaihr10@gmail.com>
2024-04-29 11:47:28 +0545

    project start

---
+++ JSON data/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace JSON_data.Controllers
+{
+	public class TaskController : Controller
+	{
+		[HttpPost("Upload")]
+		public async Task<IActionResult> uploadFile([FromForm] IFormFile file)
+		{
+			try
+			{
+				return Ok("You have uploaded your file successfully");
+			}
+			catch(Exception ex)
+			{
+				return Problem(ex.Message);
+			}
+		}
+	}
+}
